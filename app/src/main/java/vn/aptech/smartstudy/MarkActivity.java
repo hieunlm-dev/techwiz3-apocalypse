0c9027f32f02a129ac441569e3a91737a5479377
Hien Phan Vo <74083036+alioda@users.noreply.github.com>
2022-08-12 17:43:12 +0700

    update mark

--- app/src/main/java/vn/aptech/smartstudy/MarkActivity.java
+++ app/src/main/java/vn/aptech/smartstudy/MarkActivity.java
@@ -3,12 +3,24 @@ package vn.aptech.smartstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class MarkActivity extends AppCompatActivity {
-
+    private Spinner spinnerSemeter;
+    private String[] semeters = new String[]{"Semester 1","Semester 2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mark);
+        initUi();
+    }
+
+    private void initUi() {
+        spinnerSemeter = findViewById(R.id.spinnerSemeter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, semeters);
+        spinnerSemeter.setAdapter(adapter);
     }
 }
