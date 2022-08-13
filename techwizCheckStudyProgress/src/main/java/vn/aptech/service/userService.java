f06714a167eb0d80dc709ee91b52a3c0769881c0
sluuthanh <sluuthanh@gmail.com>
2022-08-13 16:06:12 +0700

    test PushNo

--- techwizCheckStudyProgress/src/main/java/vn/aptech/service/userService.java
+++ techwizCheckStudyProgress/src/main/java/vn/aptech/service/userService.java
@@ -1,56 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package vn.aptech.service;
-
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.cloud.FirestoreClient;
-import java.util.concurrent.ExecutionException;
-import org.springframework.stereotype.Service;
-import vn.aptech.Entity.User;
-
-/**
- *
- * @author Thanh Sang
- */
-@Service
-public class UserService {
-
-    public String createUser(User user) throws InterruptedException, ExecutionException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getEmail()).set(user);
-        return collectionsApiFuture.get().getUpdateTime().toString();
-    }
-
-    public User getUser(int id) throws InterruptedException, ExecutionException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("users").document(String.valueOf(id));
-        ApiFuture<DocumentSnapshot> future = documentReference.get();
-        DocumentSnapshot document = future.get();
-        User user;
-        if (document.exists()) {
-            user = document.toObject(User.class);
-            return user;
-        }
-        return null;
-    }
-
-    public String updateUser(User user) throws InterruptedException, ExecutionException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getEmail()).set(user);
-
-        return collectionsApiFuture.get().getUpdateTime().toString();
-    }
-
-    public String deleteUser(int id) throws InterruptedException, ExecutionException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> writeResult = dbFirestore.collection("users").document(String.valueOf(id)).delete();
-
-        return "Delete success"+id;
-    }
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package vn.aptech.service;
+//
+//import com.google.api.core.ApiFuture;
+//import com.google.cloud.firestore.DocumentReference;
+//import com.google.cloud.firestore.DocumentSnapshot;
+//import com.google.cloud.firestore.Firestore;
+//import com.google.cloud.firestore.WriteResult;
+//import com.google.firebase.cloud.FirestoreClient;
+//import java.util.concurrent.ExecutionException;
+//import org.springframework.stereotype.Service;
+//import vn.aptech.Entity.User;
+//
+///**
+// *
+// * @author Thanh Sang
+// */
+//@Service
+//public class UserService {
+//
+//    public String createUser(User user) throws InterruptedException, ExecutionException {
+//        Firestore dbFirestore = FirestoreClient.getFirestore();
+//        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getEmail()).set(user);
+//        return collectionsApiFuture.get().getUpdateTime().toString();
+//    }
+//
+//    public User getUser(int id) throws InterruptedException, ExecutionException {
+//        Firestore dbFirestore = FirestoreClient.getFirestore();
+//        DocumentReference documentReference = dbFirestore.collection("users").document(String.valueOf(id));
+//        ApiFuture<DocumentSnapshot> future = documentReference.get();
+//        DocumentSnapshot document = future.get();
+//        User user;
+//        if (document.exists()) {
+//            user = document.toObject(User.class);
+//            return user;
+//        }
+//        return null;
+//    }
+//
+//    public String updateUser(User user) throws InterruptedException, ExecutionException {
+//        Firestore dbFirestore = FirestoreClient.getFirestore();
+//        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getEmail()).set(user);
+//
+//        return collectionsApiFuture.get().getUpdateTime().toString();
+//    }
+//
+//    public String deleteUser(int id) throws InterruptedException, ExecutionException {
+//        Firestore dbFirestore = FirestoreClient.getFirestore();
+//        ApiFuture<WriteResult> writeResult = dbFirestore.collection("users").document(String.valueOf(id)).delete();
+//
+//        return "Delete success"+id;
+//    }
+//}
