f11a7a8097c69c9280a82824921172b966330630
sluuthanh <75598829+sluuthanh@users.noreply.github.com>
2022-08-14 14:12:47 +0700

    zz

--- app/src/main/java/vn/aptech/smartstudy/ProgressActivity.java
+++ app/src/main/java/vn/aptech/smartstudy/ProgressActivity.java
@@ -7,12 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+
 public class ProgressActivity extends AppCompatActivity {
+
+    BarChart barChartSubject;
+    PieChart pieChartSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+
+        barChartSubject = findViewById(R.id.bar_chart_subject);
+        pieChartSubject = findViewById(R.id.pie_chart_subject);
+
+        ArrayList<BarEntry> barEntries= new ArrayList<>();
+        ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
         // calling the action bar
         ActionBar actionBar = getSupportActionBar();
 
